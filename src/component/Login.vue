52134cbac3c973d5eab3a525c68fb258aee99bcd
kksy <1361350111@qq.com>
2020-12-04 16:14:39 +0800

    add login

---
+++ src/component/Login.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+
+      <!-- 头像 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+
+      <!-- 登陆表单 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="el-icon-user"
+            placeholder="用户名"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="el-icon-lock"
+            placeholder="密码"
+            type="password"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 登录选择 -->
+        <el-form-item class="user_select">
+          <el-select placeholder="请选择" v-model="loginForm.userSelect">
+            <el-option label="学生" value="student"></el-option>
+            <el-option label="老师" value="teacher"></el-option>
+            <el-option label="基地" value="base"></el-option>
+            <el-option label="研究生院" value="graduateSchool"></el-option>
+            <el-option label="管理员" value="admin"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // form databinding
+      loginForm: {
+        username: "",
+        password: "",
+        userSelect: ""
+      },
+      // the rule to check the form
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    resetLoginForm() {
+      console.log(this);
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate((valid) => {
+        if (!valid) return;
+        // const result = this.$http.post("login", this.loginForm);
+        console.log(result)
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.user_select{
+  position: absolute;
+  width: 100px;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from '../component/Login.vue'
+
 
 Vue.use(VueRouter)
 
-const routes = [
-
-]
+const routes = [{
+  path: '/',
+  redirect: '/login'
+}, {
+  path: '/login',
+  component: Login
+}]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
 
 export default router
 
+// export default new VueRouter({
+//   routes: [{
+//     path: '/login',
+//     component: Login
+//   }]
+// })
